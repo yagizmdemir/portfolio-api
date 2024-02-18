b1133a8d309ca61a3d8423d6231e87e644201504
Yağız Mehmet Demir <87898868+yagizmdemir@users.noreply.github.com>
2024-02-18 22:16:23 +0300

    init api

---
+++ server.js
@@ -0,0 +1,8 @@
+const strapi = require('@strapi/strapi');
+
+if (process.env.NODE_ENV == "development") {
+    strapi({ "autoReload": { "enabled": true } }).start();
+}
+else {
+    strapi().start();
+}
